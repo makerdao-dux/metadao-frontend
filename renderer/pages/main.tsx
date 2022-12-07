1ed22e1f8fb8d78b89e555dd54f0f60f17998dd4
Venture <rafinskipg@gmail.com>
2022-12-07 15:05:24 +0100

    Add hashrouter for electronsupport

--- renderer/pages/main.tsx
+++ renderer/pages/main.tsx
@@ -6,17 +6,16 @@ import './index.css';
 import { WagmiConfig } from 'wagmi';
 import { chains, wagmiClient } from '../modules/providers/wagmi';
 import { darkTheme, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import config from '../modules/config';
 
 import '@rainbow-me/rainbowkit/styles.css';
 
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import Home from './Home';
 import Vaults from './Vaults';
 import Delegates from './Delegates';
 import Farms from './Farms';
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Home />,
