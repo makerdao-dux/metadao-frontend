fc42652109396c01ba5043b2375e7a46b6138f04
b-pmcg <bain.pmcg@gmail.com>
2022-12-08 16:39:05 -0700

    debug fetchBalance for DAI and MKR

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,17 @@ import React, { Component, useEffect, useState } from 'react';
 import { Layout } from '../modules/layout/components/Layout';
 import Decoration from '../modules/layout/components/Decoration';
 import { fetchBalance } from '@wagmi/core';
+import { useNetwork } from 'wagmi';
 
 // TODO delete this Component, using temporarily to provide network info for debugging
-const Balances = () => {
+const Balances = ({}) => {
+  const dai = '0x6b175474e89094c44da98b954eedeac495271d0f';
+  const mkr = '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2';
   const [ethBal, setEthBal] = useState('');
+  const [mkrBal, setMkrBal] = useState('');
+  const [daiBal, setDaiBal] = useState('');
+  const network = useNetwork();
+  const chainId = network.chain?.id;
 
   useEffect(() => {
     const getBal = async () => {
@@ -16,12 +23,38 @@ const Balances = () => {
       setEthBal(balance.formatted);
     };
     getBal();
-  }, [ethBal]);
+  }, [chainId]);
+
+  useEffect(() => {
+    const getBal = async () => {
+      const balMkr = await fetchBalance({
+        address: '0x2506ead42c8c712BfA82481877D12748489612c8',
+        token: mkr
+      });
+      console.log('theMKRbal', balMkr);
+      setMkrBal(balMkr.formatted);
+    };
+    getBal();
+  }, [chainId]);
+
+  useEffect(() => {
+    const getBal = async () => {
+      const balMkr = await fetchBalance({
+        address: '0x2506ead42c8c712BfA82481877D12748489612c8',
+        token: dai
+      });
+      console.log('theMKRbal', balMkr);
+      setDaiBal(balMkr.formatted);
+    };
+    getBal();
+  }, [chainId]);
 
   return (
     <div>
       <p>temp debug info:</p>
-      <p>my bal {ethBal} </p>
+      <p>ETH balance: {ethBal} </p>
+      <p>MKR balance: {mkrBal} </p>
+      <p>DAI balance: {daiBal} </p>
     </div>
   );
 };
