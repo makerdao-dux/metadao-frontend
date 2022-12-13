4c81f42c43da03578472a99a39ce9e140031530b
Rafael Ventura <rafinskipg@gmail.com>
2022-12-13 15:36:46 +0100

    Error page and more theme

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { Layout } from '../modules/layout/components/Layout';
-import { Heading, Text } from 'theme-ui';
+import { Box, Card, Flex, Heading, Paragraph, Text } from 'theme-ui';
+import { Link } from 'react-router-dom';
 
 function ErrorPage(): React.ReactElement {
   return (
     <Layout>
-      <Heading>Not Found</Heading>
-      <Text as="h1">Where are you going?</Text>
+      <Box
+        sx={{
+          textAlign: 'center',
+          my: 6
+        }}
+      >
+        <Heading>404 - Page not found</Heading>
+
+        <Text
+          as="p"
+          sx={{
+            m: 3
+          }}
+        >
+          Don't find what you where looking for? Go back to our <Link to="/">Homepage</Link>
+        </Text>
+      </Box>
     </Layout>
   );
 }
