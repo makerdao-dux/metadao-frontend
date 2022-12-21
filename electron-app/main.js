697fa7a5d7a8c57d96977f1e5b6765ea319bc8f2
Rafael Ventura <rafinskipg@gmail.com>
2022-12-21 11:41:39 +0100

    metamask

--- electron-app/main.js
+++ electron-app/main.js
@@ -42,7 +42,7 @@ async function launchApp() {
     show: false,
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: true,
+      contextIsolation: false,
       preload: path.join(__dirname, 'preload.js')
     }
   });
