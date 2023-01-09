a223c90c2a49722ba190d2bfa2b362dfa3f1994a
Adam Goth <adamgoth@gmail.com>
2023-01-09 10:19:51 +0100

    Add oracle addresses and abis

--- apps/webapp/src/modules/contracts/index.ts
+++ apps/webapp/src/modules/contracts/index.ts
@@ -8,6 +8,11 @@ import mainnetstETHAbi from './abis/mainnet/stETH.json';
 import optimismwstETHAbi from './abis/optimism/stETH.json';
 import arbitrummwstETHAbi from './abis/arbitrumOne/stETH.json';
 import optimismOPAbi from './abis/optimism/op.json';
+import mainnetPipEthAbi from './abis/mainnet/PIP_ETH.json';
+import mainnetPipWstethAbi from './abis/mainnet/PIP_WSTETH.json';
+import mainnetPipWbtcAbi from './abis/mainnet/PIP_WBTC.json';
+
+export const ORACLE_CONTRACTS = ['PIP_ETH', 'PIP_WSTETH', 'PIP_WBTC'];
 
 export const contracts = {
   [chainId.mainnet]: {
@@ -30,6 +35,18 @@ export const contracts = {
     STETH: {
       address: '0xdfe66b14d37c77f4e9b180ceb433d1b164f0281d',
       abi: mainnetstETHAbi
+    },
+    PIP_ETH: {
+      address: '0x81FE72B5A8d1A857d176C3E7d5Bd2679A9B85763',
+      abi: mainnetPipEthAbi
+    },
+    PIP_WSTETH: {
+      address: '0xFe7a2aC0B945f12089aEEB6eCebf4F384D9f043F',
+      abi: mainnetPipWstethAbi
+    },
+    PIP_WBTC: {
+      address: '0xf185d0682d50819263941e5f4EacC763CC5C6C42',
+      abi: mainnetPipWbtcAbi
     }
   },
   [chainId.arbitrum]: {
@@ -67,6 +84,18 @@ export const contracts = {
     DSR_MANAGER: {
       address: '0xF7F0de3744C82825D77EdA8ce78f07A916fB6bE7',
       abi: DsrManagerAbi
+    },
+    PIP_ETH: {
+      address: '0x94588e35fF4d2E99ffb8D5095F35d1E37d6dDf12',
+      abi: mainnetPipEthAbi
+    },
+    PIP_WSTETH: {
+      address: '0x323eac5246d5BcB33d66e260E882fC9bF4B6bf41',
+      abi: mainnetPipWstethAbi
+    },
+    PIP_WBTC: {
+      address: '0xE7de200a3a29E9049E378b52BD36701A0Ce68C3b',
+      abi: mainnetPipWbtcAbi
     }
   }
 };
