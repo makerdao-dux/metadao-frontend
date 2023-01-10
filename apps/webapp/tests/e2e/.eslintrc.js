ed4fa5db129663fc8aeaab605ee750c70edf5a60
b-pmcg <bain.pmcg@gmail.com>
2023-01-10 14:45:58 -0700

    mock bad code for lint-staged2

--- apps/webapp/tests/e2e/.eslintrc.js
+++ apps/webapp/tests/e2e/.eslintrc.js
@@ -2,6 +2,8 @@
 const path = require('path');
 const synpressPath = path.join(process.cwd(), '/node_modules/@synthetixio/synpress');
 
+console.log('synpress PATH', synpressPath);
+
 module.exports = {
   extends: `${synpressPath}/.eslintrc.js`
 };
