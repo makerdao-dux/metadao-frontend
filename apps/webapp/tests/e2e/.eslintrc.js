2a76a83046c4fb523e63def4b390e2547f67d4e0
b-pmcg <bain.pmcg@gmail.com>
2023-01-10 13:51:08 -0700

    update synpress.config

---
+++ apps/webapp/tests/e2e/.eslintrc.js
@@ -0,0 +1,7 @@
+/* eslint-disable */
+const path = require('path');
+const synpressPath = path.join(process.cwd(), '/node_modules/@synthetixio/synpress');
+
+module.exports = {
+  extends: `${synpressPath}/.eslintrc.js`
+};
